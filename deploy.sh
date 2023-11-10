41c2d46e138d8a4cf6c8185877ead7fcc35170a5
Matt <matt.fannin@websupportctr.com>
2023-11-09 16:50:02 -0800

    removed dist from gitignot

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:kreative-dev/doba-new-products.git master:gh-pages
+git push -f git@github.com:kreative-dev/doba-new-products.git gh-pages
 cd -
